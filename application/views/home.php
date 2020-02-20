a73740f86aacbe08dcc7403d666a8f478330c898
Joël Elunda <joel.elunda.01@gmail.com>
2020-02-20 20:51:56 +0100

    contact ok

--- application/views/home.php
+++ application/views/home.php
@@ -65,10 +65,10 @@
 					<ul>
 						<li><a href="#" data-nav-section="home">Accueil</a></li>
 						<li><a href="#" data-nav-section="about">Auteurs</a></li>
-						<li><a href="#" data-nav-section="services">Blog</a></li>
-						<li><a href="#" data-nav-section="skills">Achetez nos livres</a></li>
-						<!-- <li><a href="#" data-nav-section="education">Contact</a></li>
-						<li><a href="#" data-nav-section="experience">Experience</a></li>
+						<li><a href="#" data-nav-section="buy">Achetez nos livres</a></li>
+						<li><a href="#" data-nav-section="blog">Blog</a></li>
+					    <li><a href="#" data-nav-section="contact">Contact</a></li>  
+						<!--<li><a href="#" data-nav-section="experience">Experience</a></li>
 						<li><a href="#" data-nav-section="work">Work</a></li>
 						<li><a href="#" data-nav-section="blog">Blog</a></li> -->
 					</ul>
@@ -136,7 +136,7 @@
 							<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
 								<div class="col-md-12">
 									<div class="about-desc">
-										<span class="heading-meta">A propos de nous</span>
+										<span class="heading-meta">Auteurs</span>
 										<h2 class="colorlib-heading">Qui sommes-nous ?</h2>
 										<p><strong>Hi I'm Jackson Ford</strong> On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
 										<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
@@ -220,6 +220,148 @@
 					</div>
 				</div>
 			</section>
+
+
+
+
+
+			<section   data-section="buy">
+				<div class="container my-5 py-5 z-depth-1">
+
+
+					<!--Section: Content-->
+					<section class="text-center">
+
+						<!-- Section heading -->
+						<h3 class="font-weight-bold mb-5">Product Details</h3>
+
+						<div class="row">
+						<div class="col-lg-6">
+
+							<!--Carousel Wrapper-->
+							<div id="carousel-thumb1" class="carousel slide carousel-fade carousel-thumbnails mb-5 pb-4" data-ride="carousel">
+
+							<!--Slides-->
+							<div class="carousel-inner text-center text-md-left" role="listbox">
+								<div class="carousel-item active">
+								<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/1.jpg"
+									alt="First slide" class="img-fluid">
+								</div>
+								<div class="carousel-item">
+								<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/2.jpg"
+									alt="Second slide" class="img-fluid">
+								</div>
+								<div class="carousel-item">
+								<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/20.jpg"
+									alt="Third slide" class="img-fluid">
+								</div>
+							</div>
+							<!--/.Slides-->
+
+							<!--Thumbnails-->
+							<a class="carousel-control-prev" href="#carousel-thumb1" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carousel-thumb1" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+							<!--/.Thumbnails-->
+
+							</div>
+							<!--/.Carousel Wrapper-->
+							
+							<div class="row mb-4">
+							<div class="col-md-12">
+								<div id="mdb-lightbox-ui"></div>
+								<div class="mdb-lightbox no-margin">
+								<figure class="col-md-4">
+									<a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/1.jpg"
+									data-size="1600x1067">
+									<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/1.jpg"
+										class="img-fluid">
+									</a>
+								</figure>
+								<figure class="col-md-4">
+									<a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/2.jpg"
+									data-size="1600x1067">
+									<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/2.jpg"
+										class="img-fluid">
+									</a>
+								</figure>
+								<figure class="col-md-4">
+									<a href="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/20.jpg"
+									data-size="1600x1067">
+									<img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/20.jpg"
+										class="img-fluid">
+									</a>
+								</figure>
+								</div>
+							</div>
+							</div>
+							
+						</div>
+
+						<div class="col-lg-5 text-center text-md-left">
+
+							<h2 class="h2-responsive text-center text-md-left product-name font-weight-bold dark-grey-text mb-1 ml-xl-0 ml-4">iPad PRO</h2>
+							<span class="badge badge-danger product mb-4 ml-xl-0 ml-4">bestseller</span>
+							<span class="badge badge-success product mb-4 ml-2">SALE</span>
+
+							<h3 class="h3-responsive text-center text-md-left mb-5 ml-xl-0 ml-4">
+							<span class="red-text font-weight-bold">
+								<strong>$1449</strong>
+							</span>
+							<span class="grey-text">
+								<small>
+								<s>$1789</s>
+								</small>
+							</span>
+							</h3>
+
+							<div class="font-weight-normal">
+							
+							<p class="ml-xl-0 ml-4">Quelques informations supplementaires sur le livre</p>
+
+							<p class="ml-xl-0 ml-4">
+								<strong>Storage: </strong>64GB</p>
+							<p class="ml-xl-0 ml-4">
+								<strong>Size: </strong>9.6-inch</p>
+							<p class="ml-xl-0 ml-4">
+								<strong>Resolution: </strong>2048 x 1536</p>
+							<p class="ml-xl-0 ml-4">
+								<strong>Availability: </strong>In stock</p>
+							
+							<div class="mt-5">
+								<div class="row text-center text-md-left">
+								
+									<div class="row mt-3 mb-4">
+									<div class="col-md-12 text-center text-md-left text-md-right">
+										<button class="btn btn-primary btn-rounded">
+										<i class="fas fa-cart-plus mr-2" aria-hidden="true"></i> Add to cart</button>
+									</div>
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+						</div>
+
+					</section>
+					<!--Section: Content-->
+
+
+				</div>
+
+			</section>
+			<!--Section: Content-->
+
+
+
+
 
 
 			
@@ -758,7 +900,7 @@
 				<div class="colorlib-narrow-content">
 					<div class="row">
 						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-							<span class="heading-meta">Get in Touch</span>
+							<span class="heading-meta">Nos contacts</span>
 							<h2 class="colorlib-heading">Contact</h2>
 						</div>
 					</div>
@@ -796,19 +938,19 @@
 								<div class="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInRight">
 									<form action="">
 										<div class="form-group">
-											<input type="text" class="form-control" placeholder="Name">
+											<input type="text" class="form-control" placeholder="Nom">
 										</div>
 										<div class="form-group">
 											<input type="text" class="form-control" placeholder="Email">
 										</div>
 										<div class="form-group">
-											<input type="text" class="form-control" placeholder="Subject">
+											<input type="text" class="form-control" placeholder="Sujet">
 										</div>
 										<div class="form-group">
 											<textarea name="" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
 										</div>
 										<div class="form-group">
-											<input type="submit" class="btn btn-primary btn-send-message" value="Send Message">
+											<input type="submit" class="btn btn-primary btn-send-message" value="Envoyer">
 										</div>
 									</form>
 								</div>
@@ -824,9 +966,12 @@
 	</div><!-- end:colorlib-page -->
 	
 	<!-- jQuery -->
-	<script src="<?=base_url('assets/js/jquery-3-3-1.min.js');?>"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- <script src="<?=base_url('assets/js/jquery-3-3-1.min.js');?>"></script>
 	<script src="<?=base_url('assets/js/popper.min.js');?>"></script>
-	<script src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
+	<script src="<?=base_url('assets/js/bootstrap.min.js');?>"></script> -->
 	<script src="<?=base_url('assets/js/jquery.min.js');?>"></script>
 	<!-- jQuery Easing -->
 	<script src="<?=base_url('assets/js/jquery.easing.1.3.js');?>"></script>
